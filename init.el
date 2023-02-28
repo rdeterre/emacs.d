d855b44b05499587717217ca6816a7ff81aec4d6
Romain Deterre <rdeterre@amazon.com>
2023-02-28 17:17:35 -0500

    SBT for Scala

--- init.el
+++ init.el
@@ -434,7 +434,21 @@ The app is chosen from your OS's preference."
 (add-hook 'project-find-functions #'project-override)
 
 ;; scala
-(use-package scala-mode)
+(use-package scala-mode
+  :interpreter ("scala" . scala-mode))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+   ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+   (setq sbt:program-options '("-Dsbt.supershell=false")))
 
 ;; shell-pop
 (use-package shell-pop
