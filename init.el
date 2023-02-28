ee62c65c67150f2a85081721096b5c88f10bf851
Romain Deterre <rdeterre@amazon.com>
2023-02-28 17:15:33 -0500

    Fixes to my eglot

--- init.el
+++ init.el
@@ -54,9 +54,74 @@
   :defer t
   :config
   (add-to-list 'eglot-server-programs
-               '(java-mode . ("jdtls" "--jvm-arg=-javaagent:/Volumes/brazil-pkg-cache/packages/Lombok/Lombok-1.18.x.22025.0/AL2_x86_64/DEV.STD.PTHREAD/build/lib/lombok-1.18.24.jar" "--jvm-arg=-Xbootclasspath/a:/Volumes/brazil-pkg-cache/packages/Lombok/Lombok-1.18.x.22025.0/AL2_x86_64/DEV.STD.PTHREAD/build/lib/lombok-1.18.24.jar"
-                              :initializationOptions (:extendedClientCapabilities (:classFileContentsSupport t )))))
+;               '(java-mode . ("/Users/rdeterre/Downloads/jdt-language-server-1.20.0-202302201605/bin/jdtls" "--jvm-arg=-javaagent:/Volumes/brazil-pkg-cache/packages/Lombok/Lombok-1.18.x.22025.0/AL2_x86_64/DEV.STD.PTHREAD/build/lib/lombok-1.18.24.jar" "--jvm-arg=-Xbootclasspath/a:/Volumes/brazil-pkg-cache/packages/Lombok/Lombok-1.18.x.22025.0/AL2_x86_64/DEV.STD.PTHREAD/build/lib/lombok-1.18.24.jar"
+               '(java-mode . ("jdtls" "--jvm-arg=-javaagent:/Volumes/brazil-pkg-cache/packages/Lombok/Lombok-1.18.x.22025.0/AL2_x86_64/DEV.STD.PTHREAD/build/lib/lombok-1.18.24.jar"
+                                            :initializationOptions (:extendedClientCapabilities (:classFileContentsSupport t ))))
+)
   :bind (("C-c a" . eglot-code-actions)))
+
+;; The jdt server sometimes returns jdt:// scheme for jumping to definition
+;; instead of returning a file. This is not part of LSP and eglot does not
+;; handle it. The following code enables eglot to handle jdt files.
+;; See https://github.com/yveszoundi/eglot-java/issues/6 for more info.
+(defun jdt-file-name-handler (operation &rest args)
+  "Support Eclipse jdtls `jdt://' uri scheme."
+  (let* ((uri (car args))
+         (cache-dir "/tmp/.eglot")
+         (source-file
+          (expand-file-name
+           (file-name-concat
+            cache-dir
+            (save-match-data
+              (when (string-match "jdt://contents/\\(.*?\\)/\\(.*\\)\.class\\?" uri)
+                (message "URI:%s" uri)
+                (format "%s.java" (replace-regexp-in-string "/" "." (match-string 2 uri) t t))))))))
+    (unless (file-readable-p source-file)
+      (let ((content (jsonrpc-request (eglot-current-server) :java/classFileContents (list :uri uri)))
+            (metadata-file (format "%s.%s.metadata"
+                                   (file-name-directory source-file)
+                                   (file-name-base source-file))))
+        (message "content:%s" content)
+        (unless (file-directory-p cache-dir) (make-directory cache-dir t))
+        (with-temp-file source-file (insert content))
+        (with-temp-file metadata-file (insert uri))))
+    source-file))
+
+(add-to-list 'file-name-handler-alist '("\\`jdt://" . jdt-file-name-handler))
+
+(defun jdthandler--wrap-legacy-eglot--path-to-uri (original-fn &rest args)
+  "Hack until eglot is updated.
+ARGS is a list with one element, a file path or potentially a URI.
+If path is a jar URI, don't parse. If it is not a jar call ORIGINAL-FN."
+  (let ((path (file-truename (car args))))
+    (if (equal "jdt" (url-type (url-generic-parse-url path)))
+        path
+      (apply original-fn args))))
+
+(defun jdthandler--wrap-legacy-eglot--uri-to-path (original-fn &rest args)
+  "Hack until eglot is updated.
+ARGS is a list with one element, a URI.
+If URI is a jar URI, don't parse and let the `jdthandler--file-name-handler'
+handle it. If it is not a jar call ORIGINAL-FN."
+  (let ((uri (car args)))
+    (if (and (stringp uri)
+             (string= "jdt" (url-type (url-generic-parse-url uri))))
+        uri
+     (apply original-fn args))))
+      
+
+(defun jdthandler-patch-eglot ()
+  "Patch old versions of Eglot to work with Jdthandler."
+  (interactive) ;; TODO, remove when eglot is updated in melpa
+  (unless (or (and (advice-member-p #'jdthandler--wrap-legacy-eglot--path-to-uri 'eglot--path-to-uri)
+                   (advice-member-p #'jdthandler--wrap-legacy-eglot--uri-to-path 'eglot--uri-to-path))
+              (<= 29 emacs-major-version))
+    (advice-add 'eglot--path-to-uri :around #'jdthandler--wrap-legacy-eglot--path-to-uri)
+    (advice-add 'eglot--uri-to-path :around #'jdthandler--wrap-legacy-eglot--uri-to-path)
+    (message "[jdthandler] Eglot successfully patched.")))
+
+; invoke
+(jdthandler-patch-eglot)
 
 ;; Emacs defaults
 (setq custom-file "~/.emacs.d/custom.el")
