7929af05d0b419b8bef2c7a774621e8265192e21
Romain Deterre <rdeterre@amazon.com>
2023-05-08 14:44:24 -0400

    gutter

--- init.el
+++ init.el
@@ -169,6 +169,19 @@ handle it. If it is not a jar call ORIGINAL-FN."
 ;; fonts
 (require 'init-fonts)
 
+;; git gutter
+;; See https://ianyepan.github.io/posts/emacs-git-gutter/
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
 ;; graphviz
 (use-package graphviz-dot-mode)
 
