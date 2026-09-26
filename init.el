f0cccff001dc6ccf46af94b09fb9154ead2414ed
Romain Deterre <romain.deterre@gmail.com>
2026-09-26 11:50:31 -0400

    Exclude M-' from ghostel

--- init.el
+++ init.el
@@ -998,7 +998,7 @@ This command does not push text to `kill-ring'."
   :custom
   (ghostel-keymap-exceptions
    '("C-c" "C-x" "C-u" "C-h" "M-x" "M-:" "C-\\"
-     "M-o" "M-1" "M-2" "M-3" "M-4" "M-5" "M-6")))
+     "M-o" "M-1" "M-2" "M-3" "M-4" "M-5" "M-6" "M-'")))
 
 ;; --- pdf-tools
 (use-package pdf-tools)
